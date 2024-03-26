a0b7e7d91a4336740c079edc9c0419d652af10d9
cfitech13 <amir.amiransari987@gmail.com>
2024-03-26 12:51:30 +0530

    used redis pub/sub module to ack all the server for message

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,19 +3,19 @@ import SocketService from "./services/socket"
 
 async function init() {
 
-    const sockerService = new SocketService()
+    const socketService = new SocketService()
 
     const httpServer = http.createServer()
     const PORT = process.env.PORT || 8000
 
     // attaching the socket server 
-    sockerService.io.attach(httpServer)
-
-    httpServer
+    socketService.io.attach(httpServer)
 
     httpServer.listen(PORT, ()=>{
         console.log(`Http server started on port ${PORT}`)
     })
+
+    socketService.initListener()
 }
 
 init()
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,20 +1,48 @@
 import { Server } from "socket.io"
+import Redis from "ioredis"
+require('dotenv').config()
+
+const redisCred = {
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT) || 6369,
+    username: process.env.REDIS_USERNAME || "default",
+    password: process.env.REDIS_PASSWORD || "password"
+}
+const messageChannel = "MESSAGES"
+const pub = new Redis(redisCred)
+const sub = new Redis(redisCred)
 
 class SocketService {
     private _io: Server
     constructor() {
         console.log("Init socket service...")
-        this._io = new Server()
+        this._io = new Server({
+            cors: {
+                allowedHeaders: ["*"],
+                origin: "*"
+            }
+        })
+        sub.subscribe(messageChannel)
     }
 
     public initListener() {
         const io = this._io
-        io.on("connect", (socket) => {
+        console.log("Init socket listeners...")
+        io.on("connection", (socket) => {
             console.log(`New Socket Connected ${socket.id}`)
 
             socket.on("event:message", async ({ message }: { message: string }) => {
                 console.log('New message rec', message)
+                //publish this message to redis
+                await pub.publish(messageChannel, JSON.stringify({ message }))
             })
+        })
+
+        sub.on("message", (channel, message) => {
+            if (channel === messageChannel) {
+                console.log("New message from redis", message)
+                io.emit("message", message)
+            }
         })
     }
 
